258e797d265b96570406851fcd312ab3fa6d0f3b
Ranjay Krishna <ranjaykrishna@gmail.com>
2019-07-16 20:24:07 -0700

    adding code to render template.

---
+++ scripts/render_template.py
@@ -0,0 +1,24 @@
+"""Script to render a given EasyTurk task.
+"""
+
+import argparse
+
+from amt import AMT
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--template', required=True)
+    parser.add_argument('--output', required=True)
+    args = parser.parse_args()
+
+    # Compile the template.
+    mturk = AMT()
+    env = mturk.get_jinja_env()
+    template = env.get_template(args.template)
+
+    html = template.render({'input': ''})
+
+    # Save to output.
+    with open(args.output, 'w') as f:
+        f.write(html)
