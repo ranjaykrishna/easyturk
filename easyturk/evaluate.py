5b32c046657fa11413c451956d08b1515ea031b8
Ranjay Krishna <ranjaykrishna@gmail.com>
2019-07-31 18:29:36 -0700

    adding evaluation server that allows users to easily navigate and go through and approve and reject work done by workers.

---
+++ easyturk/evaluate.py
@@ -0,0 +1,89 @@
+"""Script to render a completed EasyTurk task.
+"""
+
+from flask import Flask
+from flask import render_template
+from flask import request
+
+import json
+import os
+
+# Global server variables.
+app = Flask(__name__)
+
+
+def get_e_filename(filename):
+    """Modifies the filename to include _e_ in front.
+
+    Args:
+        filename: A filename (ex, directory/filename.json)
+
+    Returns:
+        A string directory/_e_filename.json
+    """
+    dirname = os.path.dirname(filename)
+    index = len(dirname) + 1
+    dirname = filename[0:index]
+    filename = filename[index:]
+    return dirname + '_e_' + filename
+
+
+def convert(results):
+    """Converts the results to include metadata for navigation.
+
+    Args:
+        results: A dictionary mapping from hit_id to hit data.
+
+    Returns:
+        A dictionary containing:
+            - hits: All hits ordered by workers.
+            - workers: Mapping from worker_id to indices into hits.
+    """
+    # Organize all the data by worker id.
+    worker_map = {}
+    for hit_id, hit in results.items():
+        for assignment in hit:
+            worker_id = assignment['worker_id']
+            if worker_id not in worker_map:
+                worker_map[worker_id] = []
+            worker_map[worker_id].append(assignment)
+
+    # Create the hits and worker index mapping structures.
+    hits = []
+    worker_indices = {}
+    worker_ids = []
+    for worker_id, assignments in worker_map.items():
+        worker_indices[worker_id] = range(
+                len(hits), len(hits) + len(assignments))
+        hits.extend(assignments)
+        worker_ids.append(worker_id)
+    output = {'hits': hits,
+              'workers': worker_indices,
+              'worker_ids': worker_ids}
+    return output
+
+
+@app.route('/task')
+def task():
+    # Compile the template.
+    task = request.args['task']
+    results_file = request.args['results']
+    eresults_file = get_e_filename(results_file)
+    if os.path.exists(eresults_file):
+        results = json.load(open(eresults_file))
+    else:
+        results = json.load(open(results_file))
+        results = convert(results)
+        json.dump(results, open(eresults_file, 'w'))
+    return render_template(
+            'evaluation/' + task,
+            results=results)
+
+
+@app.route('/')
+def home():
+    return render_template('evaluation/home.html')
+
+
+if __name__=='__main__':
+    app.run()
